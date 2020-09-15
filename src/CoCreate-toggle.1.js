1cf32be26457db7de7731bb9e884cd1cb6d9f827
Frank Pagan <frank@cocreate.app>
2020-09-15 19:08:23 +0000

    Added selected script and demo

--- src/CoCreate-toggle.1.js
+++ src/CoCreate-toggle.1.js
@@ -31,60 +31,37 @@
 
 
 
-let list_data_toogles = document.querySelectorAll('[data-toggle]');
 
-list_data_toogles.forEach(elem=>{
-     elem.addEventListener("click",function(e){
-        let values = elem.dataset['toggle'].split(',').map(function(x){return x.trim()});
-        let target = elem.dataset['toggle_attribute'] ? elem.dataset['toggle_attribute'] : 'class';
-        let target_toggle_element = elem.dataset['toggle_element'] ? elem.dataset['toggle_element'] : false;
-        let toggle_closest = elem.dataset['toggle_closest'];
-        let elem_change = elem;
-        let array_2 = elem.getAttribute(target).split(' ').map(function(x){return x.trim()});
-        let  toggle_tmp = get_val_toggle_attributes(values,array_2);
-        let tmp_val = get_value(values,toggle_tmp);
-        if(typeof(toggle_closest) != 'undefined'){
-            elem_change = elem.closest(toggle_closest);
-        }
-        if(target_toggle_element){
-                document.querySelectorAll(target_toggle_element).forEach(elem_change=>{
-                    change_values(target,toggle_tmp,values,tmp_val,elem,elem_change);
-                });
-        }else{
-            if(change_values(target,toggle_tmp,values,tmp_val,elem,elem_change))
-                return
-        }
-    });//end click
-});//end for
-
-
-function get_val_toggle_attributes(values,array_2){
-    console.log("First Array ",values)
-    console.log("Segundo Array ",array_2)
-    var array_first = values;
-    var array_second = array_2;
+document.addEventListener("click",function(e){
+    if(!e.target.getAttribute('data-toggle'))
+     return;
+    let elem = e.target;
+    let values = elem.dataset['toggle'].split(',').map(function(x){return x.trim()});
+    let target = elem.dataset['toggle_attribute'] ? elem.dataset['toggle_attribute'] : 'class';
+    let toggle_tmp = elem.dataset['toggle_tmp'] ?  elem.dataset['toggle_tmp'] : '';
+    let toggle_closest = elem.dataset['toggle_closest'];
+    let elem_change = elem;
     
-    var array_intersection = array_first.filter(function(x) {
-    	// checking second array contains the element "x"
-    	if(array_second.indexOf(x) != -1)
-    		return true;
-    	else
-    		return false;
-    });
-    let retorno = (array_intersection.length ) ? array_intersection[0] : '';
-    console.log("retorno ",retorno)
-    return retorno
-        
-    //console.log("Intercept ",array_intersection);
+    let tmp_val = get_value(values,toggle_tmp);
+    if(typeof(toggle_closest) != 'undefined'){
+        elem_change = elem.closest(toggle_closest);
+    }
+    if(change_values(target,toggle_tmp,values,tmp_val,elem,elem_change))
+        return
     
-}
+    elem.dataset['toggle_tmp'] = tmp_val;
+    //console.log("nextValue ",tmp_val)
+    
+});//end click
+
+
 
 function change_values(target,toggle_tmp,values,tmp_val,elem,elem_change){
     console.log("elem_change",elem_change)
     if(target=='class' && toggle_tmp!=''){
             elem_change.classList.remove(toggle_tmp)
             if(values.length==1){
-                //elem.dataset['toggle_tmp'] = '';
+                elem.dataset['toggle_tmp'] = '';
                 return true
             }
         }
